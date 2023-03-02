fcb305ed1a6723dd19792bd3e527281e6e45d80d
CindyMisoi <crmisoi2k@gmail.com>
2023-03-02 19:12:35 +0300

    edited application_contoller

--- app/controllers/application_contoller.rb
+++ app/controllers/application_contoller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < Sinatra::Base
-end
+    set :default_content_type, 'application/json'
+    
+    # Add routes
+    get '/users' do 
+      users = User.all.order(created_at: :asc)
+      users.to_json
+    end
+    post '/users' do
+      user = User.create(
+        username: params[:username],
+        email: params[:email],
+        password: params[:password]
+      )
+      user.to_json
+    end
+    patch '/users/:id' do
+      user = User.find(params[:id])
+      user.update(
+        username: params[:username],
+        email: params[:email],
+        password: params[:password]
+      )
+      user.to_json
+    end
+  
+    delete '/users/:id' do
+      user = User.find(params[:id])
+      user.destroy
+      user.to_json
+    end
+  end
+  
