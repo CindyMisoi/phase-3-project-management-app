a7990ca3bf67ad1343f366d700a7e439b772b620
CindyMisoi <crmisoi2k@gmail.com>
2023-03-02 19:33:01 +0300

    edited application_contoller

--- app/controllers/application_contoller.rb
+++ app/controllers/application_contoller.rb
@@ -29,5 +29,63 @@ class ApplicationController < Sinatra::Base
       user.destroy
       user.to_json
     end
+    # Members
+    get '/members' do 
+      members = Member.all.order(created_at: :asc)
+      members.to_json
+    end
+    post '/members' do
+      member = Member.create(
+        name: params[:name],
+        email: params[:email],
+        user_id: params[:user_id]
+        project_id: params[:project_id]
+      )
+      member.to_json
+    end
+    patch '/members/:id' do
+      member = Member.find(params[:id])
+      member.update(
+        name: params[:name],
+        email: params[:email],
+        user_id: params[:user_id]
+        project_id: params[:project_id]
+      )
+      member.to_json
+    end
+  
+    delete '/members/:id' do
+      member = Member.find(params[:id])
+      member.destroy
+      member.to_json
+    end
+    # Projects
+    get '/projects' do 
+      projects = Project.all.order(created_at: :asc)
+      projects.to_json
+    end
+    post '/projects' do
+      project = Project.create(
+        name: params[:name],
+        title: params[:title],
+        description: params[:description]
+      )
+      project.to_json
+    end
+    patch '/projects/:id' do
+      project = Project.find(params[:id])
+      project.update(
+        name: params[:name],
+        title: params[:title],
+        description: params[:description]
+      )
+      project.to_json
+    end
+  
+    delete '/projects/:id' do
+      project = Project.find(params[:id])
+      project.destroy
+      project.to_json
+    end
   end
   
