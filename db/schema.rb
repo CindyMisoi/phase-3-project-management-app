0b58034aaef22f8634c79b44fc800a3c2e2df42c
CindyMisoi <crmisoi2k@gmail.com>
2023-03-04 18:52:42 +0300

    added status, date and author columns to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_151615) do
+ActiveRecord::Schema.define(version: 2023_03_04_154942) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2023_03_04_151615) do
     t.string "name"
     t.string "title"
     t.string "description"
+    t.string "status"
+    t.datetime "date"
+    t.string "author"
   end
 
   create_table "users", force: :cascade do |t|
